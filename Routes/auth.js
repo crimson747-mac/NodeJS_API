d2e2550072dcdad6bc8d164c67818c934e056400
crimson747-mac <58840453+crimson747-mac@users.noreply.github.com>
2020-07-22 01:33:53 +0900

    Validation & Hashing

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,16 +1,31 @@
 const router = require("express").Router();
 const User = require("../model/User");
+const bcrypt = require("bcryptjs");
+const { registerValidation, loginValidation } = require("../validation");
 
 router.post("/register", async (req, res) => {
+  // Lets Validate the data before we a user
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // Checking if the user is alreay in the database
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send("Email already exists");
+
+  // HASH Passwords
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+  // Create a new user
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
+    password: hashedPassword,
   });
 
   try {
     const savedUser = await user.save();
-    res.send(savedUser);
+    res.send({ user: savedUser._id });
   } catch (err) {
     res.status(400).send(err);
   }
