db3337eec9b58623e4af8a78270ade073a4a7b70
crimson747-mac <58840453+crimson747-mac@users.noreply.github.com>
2020-07-22 00:36:12 +0900

    basic

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+// Import Routes
+const authRoute = require("./Routes/auth");
+
+dotenv.config();
+
+// Connect to DB
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  () => console.log("connected to db")
+);
+
+// Route Middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/api/user", authRoute);
+
+app.listen(3000, () => console.log("http://localhost:3000"));
